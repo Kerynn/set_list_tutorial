6cb668218a8687bd5783ab12fcbf79a242437d6f
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-01 17:20:14 -0800

    Adds artist new and create

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,14 @@ class ArtistsController < ApplicationController
   def show 
     @artist = Artist.find(params[:artist_id])
   end 
+
+  def new 
+  end 
+
+  def create 
+    artist = Artist.create(name: params[:name])
+    redirect_to "/artists"
+
+    # IF following actual test from beginning of forms video: redirect_to "/artists/#{artist.id}"
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
+  get '/artists/new', to: 'artists#new'
   get '/artists/:artist_id/songs', to: 'artist_songs#index'
   get '/songs/:id/songs', to: 'songs#index'
   get '/artists', to: 'artists#index'
   get '/artists/:artist_id', to: 'artists#show'
+  post '/artists', to: 'artists#create'
 end

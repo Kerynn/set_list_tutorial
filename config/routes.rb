2ad48b7cd640d241baa2e09c5e45d1b78093f41d
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-01 17:56:50 -0800

    Adds edit functionality to artists

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
   get '/artists/new', to: 'artists#new'
+  get '/artists/:id/edit', to: 'artists#edit'
   get '/artists/:artist_id/songs', to: 'artist_songs#index'
   get '/songs/:id/songs', to: 'songs#index'
   get '/artists', to: 'artists#index'
   get '/artists/:artist_id', to: 'artists#show'
   post '/artists', to: 'artists#create'
+  patch '/artists/:id', to: 'artists#update'
 end

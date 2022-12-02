7d981586738c9b6a37c8a10b0b76635280f3cfaa
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-01 17:23:26 -0800

    refactors artist create to use strong params

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,9 +11,13 @@ class ArtistsController < ApplicationController
   end 
 
   def create 
-    artist = Artist.create(name: params[:name])
+    artist = Artist.create(artist_params)
     redirect_to "/artists"
 
     # IF following actual test from beginning of forms video: redirect_to "/artists/#{artist.id}"
+  end 
+
+  def artist_params 
+    params.permit(:name)
   end 
 end 
